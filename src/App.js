cf36ab977e1d65fb451d48ca1623b634711342ce
unknown <piyushraval2474@gmail.com>
2023-12-10 21:04:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { lazy, Suspense } from 'react';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//components
+import UserProvider from './context/UserProvider';
+import AccountProvider from './context/AccountProvider';
+
+import Loader from './components/loader/Loader';
+
+const Messenger = lazy(() => import('./components/Messenger'));
+
+function App() {
+
+  const clientId = "154096238935-ag96p7m7bb4b6h47f3su50rbd3q9ng3e.apps.googleusercontent.com";
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <UserProvider>
+        <AccountProvider>
+          <Suspense fallback={<Loader />}>
+            <Messenger/>
+          </Suspense>
+        </AccountProvider>
+      </UserProvider>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
